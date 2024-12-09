b6c0639c5ba073b8b650dfc40e6e58b2fef59434
MevanKoda <Mevannandula@gmail.com>
2024-12-09 17:14:13 +0530

    Fixed some bugs in main.js

--- main.js
+++ main.js
@@ -1,39 +1,36 @@
-let tabs = []
-const inputEl = document.getElementById('input-el')
-const inputBtn = document.getElementById('input-btn')
-const tabBtn = document.getElementById('tab-btn')
-const delBtn = document.getElementById('del-btn')
-const outputEl = document.getElementById('output-el')
+let tabs = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
+const delBtn = document.getElementById("del-btn");
+const outputEl = document.getElementById("output-el");
+const tabsFromLocalStorage = JSON.parse(localStorage.getItem("tabs"));
 
-
-
-
-
-
-
-inputBtn.addEventListener("click", function(){
-    console.log('clicked')
-    
-    render()
-})
-
-function render(){
-    tabs.push(inputEl.value)
-    console.log(tabs)
-    let list = ''
-    for(i=0; i<tabs.length; i++){
-        list += `<li class="mt-2 text-2xl font-bold list-none">
+if (tabsFromLocalStorage) {
+  tabs = tabsFromLocalStorage;
+  render();
+}
+function render() {
+  console.log(tabs);
+  let listItem = "";
+  for (i = 0; i < tabs.length; i += 1) {
+    listItem += `<li class="mt-2 text-2xl font-bold list-none">
         <a href = "${tabs[i]}" target="_blank">${tabs[i]}</a>
-        </li>`
-    }
-    outputEl.innerHTML = list
-    inputEl.value = " "
-   
+        </li>`;
+  }
+  outputEl.innerHTML = listItem;
+  inputEl.value = " ";
 }
 
-delBtn.addEventListener("click", function(){
-    tabs = []
-    outputEl.innerHTML = tabs
-    
-})
+inputBtn.addEventListener("click", function () {
+  tabs.push(inputEl.value);
+  console.log("clicked");
+  localStorage.setItem("tabs", JSON.stringify(tabs));
+  render();
+});
 
+delBtn.addEventListener("click", function () {
+  tabs = [];
+  outputEl.innerHTML = [];
+  localStorage.clear();
+});
