d635c501ecc653c1727abfeeee6c842bec3b7610
MevanKoda <Mevannandula@gmail.com>
2024-12-09 18:32:01 +0530

    Fixed some bugs

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-let tabs = [];
+let myTabs = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const tabBtn = document.getElementById("tab-btn");
 const delBtn = document.getElementById("del-btn");
 const outputEl = document.getElementById("output-el");
-const tabsFromLocalStorage = JSON.parse(localStorage.getItem("tabs"));
+const tabsFromLocalStorage = JSON.parse(localStorage.getItem("myTabs"));
 
 if (tabsFromLocalStorage) {
-  tabs = tabsFromLocalStorage;
+  myTabs = tabsFromLocalStorage;
   render();
 }
 function render() {
-  console.log(tabs);
+  console.log(myTabs);
   let listItem = "";
-  for (i = 0; i < tabs.length; i += 1) {
+  for (i = 0; i < myTabs.length; i += 1) {
     listItem += `<li class="mt-2 text-1xl font-bold list-none">
-        <a href = "${tabs[i]}" target="_blank">${tabs[i]}</a>
+        <a href = "${myTabs[i]}" target="_blank">${myTabs[i]}</a>
         </li>`;
   }
   outputEl.innerHTML = listItem;
@@ -23,15 +23,24 @@ function render() {
 }
 
 inputBtn.addEventListener("click", function () {
-  tabs.push(inputEl.value);
+  myTabs.push(inputEl.value);
   console.log("clicked");
-  localStorage.setItem("tabs", JSON.stringify(tabs));
+  localStorage.setItem("myTabs", JSON.stringify(myTabs));
   render();
 });
 
 delBtn.addEventListener("click", function () {
-  tabs = [];
+  myTabs = [];
   outputEl.innerHTML = [];
   localStorage.clear();
 });
 
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    let url = tabs[0].url;
+    console.log(url);
+    myTabs.push(url);
+    localStorage.setItem("myTabs", JSON.stringify(myTabs));
+    render();
+  });
+});
